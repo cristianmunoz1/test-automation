587d8c9e428c3a56d0f5eeff06537d3f5032bf8c
Cristian Daniel Muñoz Botero <cristian.munoz3@udea.edu.co>
2024-11-04 21:01:12 -0500

    Se agregan 2 test para los get de la API

--- src/test/java/com/apimovies/apimovies/stepdefinitions/GetStepDefinitions.java
+++ src/test/java/com/apimovies/apimovies/stepdefinitions/GetStepDefinitions.java
@@ -1,33 +1,79 @@
 package com.apimovies.apimovies.stepdefinitions;
 
-import com.apimovies.apimovies.models.TestData;
-import com.apimovies.apimovies.tasks.Load;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import net.serenitybdd.screenplay.actors.OnStage;
-import org.junit.Assert;
-
-import java.util.List;
-import java.util.Map;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.text.MatchesPattern.matchesPattern;
 
 public class GetStepDefinitions {
 
+    private RequestSpecification request;
+    private Response response;
+    private int movieId;
+
+
     @Given("I am connected to the movie application")
-    public void iAmConnectedToTheMovieApplication(List<Map<String, String>> data) {
-        OnStage.theActorInTheSpotlight().wasAbleTo(
-                Load.testData(data.get(0))
-        );
+    public void iAmConnectedToTheMovieApplication() {
+        request = given()
+                .baseUri("http://localhost:8080")
+                .contentType("application/json");
+
     }
 
     @When("I send a specific movie's id to the movie API")
     public void iSendASpecificMovieIdToTheMovieAPI() {
-        System.out.println("Datos " + TestData.getData());
+        movieId = 1;
+        response = request
+                .when()
+                .get("/movies/" + movieId);
     }
 
-    @Then("I get all the information about that movie")
+    @Then("I get all the information about that movie in the correct format")
     public void iGetAllTheInformationAboutThatMovie() {
-        Assert.assertNotNull("Movie response should not be null");
+        response.then()
+                .statusCode(200)
+                .body("id", notNullValue())
+                .body("title", notNullValue())
+                .body("description", notNullValue())
+                .body("releaseDate", matchesPattern("\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}"))
+                .body("availableOnDvd", isA(Boolean.class));
+
+        response.then().assertThat()
+                .body("$", hasKey("id"))
+                .body("$", hasKey("title"))
+                .body("$", hasKey("description"))
+                .body("$", hasKey("releaseDate"))
+                .body("$", hasKey("availableOnDvd"));
+
+    }
+
+    @When("I request all movies from the API")
+    public void iRequestAllMoviesFromTheAPI() {
+        response = request
+                .when()
+                .get("/movies");
+    }
+    @Then("I get a list of movies in the correct format")
+    public void iGetAListOfMoviesInTheCorrectFormat() {
+        response.then()
+                .statusCode(200)
+                .body("", isA(java.util.List.class))
+                .body("[0]", hasKey("id"))
+                .body("[0]", hasKey("title"))
+                .body("[0]", hasKey("description"))
+                .body("[0]", hasKey("releaseDate"))
+                .body("[0]", hasKey("availableOnDvd"));
+
+        response.then().assertThat()
+                .body("every { it.id != null }", is(true))
+                .body("every { it.title != null }", is(true))
+                .body("every { it.description != null }", is(true))
+                .body("every { it.releaseDate =~ /\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}/ }", is(true))
+                .body("every { it.availableOnDvd in [true, false] }", is(true));
     }
 
 }
--- src/test/resources/features/get_movie.feature
+++ src/test/resources/features/get_movie.feature
@@ -3,10 +3,13 @@
 
 Feature: As an user of the movie application, I want to get all the information about a specific movie
 
+Background:
+  Given I am connected to the movie application
 
-  Scenario: Get information about one specific movie sending its id
-    Given I am connected to the movie application
-      | id |
-      | 1  |
+  Scenario: Get a specific movie
     When I send a specific movie's id to the movie API
-    Then I get all the information about that movie
+    Then I get all the information about that movie in the correct format
+
+  Scenario: Get all movies
+    When I request all movies from the API
+    Then I get a list of movies in the correct format
